609762f76192413def54629fddcc8d14878f1c3b
aprendiz <aprendiz@serpro.gov.br>
2017-08-04 15:13:20 -0300

    Criar a tabela de locais #2

---
+++ data/erm/econference.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Tempo de geração: 04/08/2017 às 15:07
+-- Versão do servidor: 5.7.18-0ubuntu0.16.04.1
+-- Versão do PHP: 7.0.18-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `econference`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `locais`
+--
+
+CREATE TABLE `locais` (
+  `codigo` int(10) UNSIGNED NOT NULL,
+  `nome` varchar(30) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Índices de tabelas apagadas
+--
+
+--
+-- Índices de tabela `locais`
+--
+ALTER TABLE `locais`
+  ADD PRIMARY KEY (`codigo`),
+  ADD KEY `nome` (`nome`);
+
+--
+-- AUTO_INCREMENT de tabelas apagadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `locais`
+--
+ALTER TABLE `locais`
+  MODIFY `codigo` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
