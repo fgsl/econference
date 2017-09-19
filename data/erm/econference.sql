bcf1e5d813b9bf605caf7a5367e0b46b45ac4191
Flávio Gomes da Silva Lisboa <flavio.lisboa@serpro.gov.br>
2017-09-19 13:10:57 -0300

    Reparar script de criação das tabelas #48

--- data/erm/econference.sql
+++ data/erm/econference.sql
@@ -26,56 +26,12 @@ SET time_zone = "+00:00";
 -- Estrutura para tabela `locais`
 --
 
-CREATE TABLE `locais` (
-  `codigo` int(10) UNSIGNED NOT NULL,
-  `nome` varchar(30) COLLATE utf8_unicode_ci NOT NULL
+CREATE TABLE IF NOT EXISTS `locais` (
+  `codigo` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`codigo`),
+  KEY `nome` (`nome`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Índices de tabelas apagadas
---
-
---
--- Índices de tabela `locais`
---
-ALTER TABLE `locais`
-  ADD PRIMARY KEY (`codigo`),
-  ADD KEY `nome` (`nome`);
-
---
--- AUTO_INCREMENT de tabelas apagadas
---
-
---
--- AUTO_INCREMENT de tabela `locais`
---
-ALTER TABLE `locais`
-  MODIFY `codigo` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Tempo de geração: 17/08/2017 às 09:39
--- Versão do servidor: 5.7.18-0ubuntu0.16.04.1
--- Versão do PHP: 7.0.18-0ubuntu0.16.04.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `econference`
---
 
 -- --------------------------------------------------------
 
@@ -83,8 +39,8 @@ SET time_zone = "+00:00";
 -- Estrutura para tabela `participantes`
 --
 
-CREATE TABLE `participantes` (
-  `codigo` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `participantes` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
   `usuario` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
   `nome` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
@@ -92,221 +48,99 @@ CREATE TABLE `participantes` (
   `telefone` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `instituicao` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
   `cpf` varchar(13) COLLATE utf8_unicode_ci NOT NULL,
-  `passaporte` varchar(10) COLLATE utf8_unicode_ci NOT NULL
+  `passaporte` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`codigo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Índices de tabelas apagadas
---
+
+-- --------------------------------------------------------
 
 --
--- Índices de tabela `participantes`
---
-ALTER TABLE `participantes`
-  ADD PRIMARY KEY (`codigo`);
-
---
--- AUTO_INCREMENT de tabelas apagadas
+-- Estrutura para tabela `sediadoras`
 --
 
---
--- AUTO_INCREMENT de tabela `participantes`
---
-ALTER TABLE `participantes`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-CREATE TABLE `sediadoras` (
-  `codigo` int(11) NOT NULL,
-  `nome` varchar(30) COLLATE utf8_unicode_ci NOT NULL
+CREATE TABLE IF NOT EXISTS `sediadoras` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`codigo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Índices de tabelas apagadas
---
+
+-- --------------------------------------------------------
 
 --
--- Índices de tabela `sediadoras`
---
-ALTER TABLE `sediadoras`
-  ADD PRIMARY KEY (`codigo`);
-
---
--- AUTO_INCREMENT de tabelas apagadas
+-- Estrutura para tabela `perfis`
 --
 
---
--- AUTO_INCREMENT de tabela `sediadoras`
---
-ALTER TABLE `sediadoras`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-CREATE TABLE `perfis` (
-  `codigo` int(11) NOT NULL,
-  `nome` varchar(30) COLLATE utf8_unicode_ci NOT NULL
+CREATE TABLE IF NOT EXISTS `perfis` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`codigo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Índices de tabelas apagadas
---
+
+-- --------------------------------------------------------
 
 --
--- Índices de tabela `perfis`
---
-ALTER TABLE `perfis`
-  ADD PRIMARY KEY (`codigo`);
-
---
--- AUTO_INCREMENT de tabelas apagadas
+-- Estrutura para tabela `permissoes`
 --
 
---
--- AUTO_INCREMENT de tabela `perfis`
---
-ALTER TABLE `perfis`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE IF NOT EXISTS `permissoes` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-<<<<<<< HEAD
-CREATE TABLE `permissoes` (
-=======
-CREATE TABLE `trabalhos` (
-  `codigo` int(11) NOT NULL,
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `trabalhos`
+--
+
+CREATE TABLE IF NOT EXISTS `trabalhos` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
   `resumo` varchar(300) COLLATE utf8_unicode_ci NOT NULL,
   `categoria` tinyint(4) NOT NULL,
-  `tipo` tinyint(4) NOT NULL
+  `tipo` tinyint(4) NOT NULL,
+  PRIMARY KEY (`codigo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Índices de tabelas apagadas
---
+-- --------------------------------------------------------
 
 --
--- Índices de tabela `trabalhos`
---
-ALTER TABLE `trabalhos`
-  ADD PRIMARY KEY (`codigo`);
-
---
--- AUTO_INCREMENT de tabelas apagadas
+-- Estrutura para tabela `categorias`
 --
 
---
--- AUTO_INCREMENT de tabela `trabalhos`
---
-ALTER TABLE `trabalhos`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-CREATE TABLE `categorias` (
->>>>>>> 69bfedb4c708f625cc2a295da7d272a498f3e545
-  `codigo` int(11) NOT NULL,
-  `nome` varchar(80) COLLATE utf8_unicode_ci NOT NULL
+CREATE TABLE IF NOT EXISTS `categorias` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`codigo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Índices de tabelas apagadas
---
+-- --------------------------------------------------------
 
 --
-<<<<<<< HEAD
--- Índices de tabela `permissoes`
---
-ALTER TABLE `permissoes`
-=======
--- Índices de tabela `categorias`
---
-ALTER TABLE `categorias`
->>>>>>> 69bfedb4c708f625cc2a295da7d272a498f3e545
-  ADD PRIMARY KEY (`codigo`);
-
---
--- AUTO_INCREMENT de tabelas apagadas
+-- Estrutura para tabela `categorias`
 --
 
---
-<<<<<<< HEAD
--- AUTO_INCREMENT de tabela `permissoes`
---
-ALTER TABLE `permissoes`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-=======
--- AUTO_INCREMENT de tabela `categorias`
---
-ALTER TABLE `categorias`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-CREATE TABLE `usuarios` (
-  `codigo` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `codigo_perfil` int(11) NOT NULL
+  `codigo_perfil` int(11) NOT NULL,
+  PRIMARY KEY (`codigo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Índices de tabelas apagadas
---
+-- --------------------------------------------------------
 
 --
--- Índices de tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`codigo`);
-
---
--- AUTO_INCREMENT de tabelas apagadas
+-- Estrutura para tabela `grades`
 --
 
---
--- AUTO_INCREMENT de tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-CREATE TABLE `grades` (
-  `codigo` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `grades` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
   `codigo_trabalho` int(11) NOT NULL,
   `data` date NOT NULL,
   `horario` time NOT NULL,
-  `codigo_local` int(11) NOT NULL
+  `codigo_local` int(11) NOT NULL,
+  PRIMARY KEY (`codigo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Índices de tabelas apagadas
---
-
---
--- Índices de tabela `grades`
---
-ALTER TABLE `grades`
-  ADD PRIMARY KEY (`codigo`);
-
---
--- AUTO_INCREMENT de tabelas apagadas
---
-
---
--- AUTO_INCREMENT de tabela `grades`
---
-ALTER TABLE `grades`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
