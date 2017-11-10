165b03da3bda33658f2e59a464d4a6383254f5d7
Flávio Gomes da Silva Lisboa <flavio.lisboa@serpro.gov.br>
2017-11-10 11:14:22 -0200

    Aperfeiçoa script de criação das tabelas #82

--- scripts/createdatabase.php
+++ scripts/createdatabase.php
@@ -3,6 +3,11 @@
 use Zend\Db\Sql\Ddl\CreateTable;
 use Zend\Db\Adapter\Adapter;
 use Zend\Db\Sql\Sql;
+use Zend\Db\Metadata\Metadata;
+
+
+error_reporting(E_ERROR);
+
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
@@ -19,9 +24,20 @@ $schema = (array) require 'data/erm/databaseschema.php';
 
 $sql = new Sql($adapter);
 
+echo "\n" . str_repeat('=',80) . "\n";
+echo "\nSistema de Gestão de Conferências\n";
+echo "\n" . str_repeat('=',80) . "\n";
 echo "\nCriando tabelas...\n";
 
+$metadata = new Metadata($adapter);
+$tableNames = $metadata->getTableNames();
+
 foreach($schema as $tableName => $tableSchema){
+    if (in_array($tableName,$tableNames)) {
+        echo "\n Tabela $tableName já existe!\n";
+        continue;
+    } 
+
     $table = new CreateTable($tableName);
     foreach($tableSchema['fields'] as $fieldName => $field){
         $field[3] = (isset($field[3]) ? $field[3] : []);
@@ -33,5 +49,5 @@ foreach($schema as $tableName => $tableSchema){
     }        
     echo "\n" . $sql->getSqlStringForSqlObject($table) . "\n";
     
-    $adapter->query($sql->getSqlStringForSqlObject($table), $adapter::QUERY_MODE_EXECUTE);    
+    $adapter->query($sql->getSqlStringForSqlObject($table), $adapter::QUERY_MODE_EXECUTE);  
 }
