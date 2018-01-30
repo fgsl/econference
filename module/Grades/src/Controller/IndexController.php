8fd45743d164d000b83a34d0bf7f4f74b816cd5f
aprendiz <aprendiz@serpro.gov.br>
2018-01-30 11:23:27 -0200

    Redução do módulo Grades #98

--- module/Grades/src/Controller/IndexController.php
+++ module/Grades/src/Controller/IndexController.php
@@ -1,68 +1,38 @@
 <?php
 /**
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @link      http://github.com/fgsl/econference for the canonical source repository
+ * @copyright Copyleft 2017 FTSL. (http://www.ftsl.org.br)
+ * @license   https://www.gnu.org/licenses/agpl-3.0.en.html GNU Affero General Public License
  */
-
 namespace Grades\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
-use Zend\View\Model\ViewModel;
-use Grades\Model\Grade;
-use Application\DatabaseHelper;
+use Application\Controller\AbstractCrudController;
 
-class IndexController extends AbstractActionController
+class IndexController extends AbstractCrudController
 {
-	private $sm;
+	protected $mainTableFactory = 'CategoriaTable';
 	
-	public function __construct($sm)
-	{
-		$this->sm = $sm;
-	}
+	protected $rowsObjectName = 'categorias';
 	
-    public function indexAction()
-    {
-    	$grades = $this->sm->get('GradeTable')->getAll();
-        return new ViewModel(['grades'=>$grades]);
-    }
-    
-    public function editAction()
-    {
-    	$codigo = $this->params('codigo');
-    	if (is_null($codigo)){
-    		$grade = new Grade();
-    	} else {    	
-    		$grade = $this->sm->get('GradeTable')->getOne($codigo);
-    		
-    	}
-    	$trabalhos = $this->sm->get('TrabalhoTable')->getAll();
-    	$this->sm->get('Zend\Db\Adapter')->getDriver()->getConnection()->disconnect();
-    	$this->sm->get('Zend\Db\Adapter')->getDriver()->getConnection()->connect();
-    	$locais = $this->sm->get('LocalTable')->getAll();
-    	return new ViewModel(['grade' => $grade,'trabalhos' => $trabalhos,'locais' => $locais]);
-    }
-    
-    public function saveAction()
+	protected $primaryKeyName = 'codigo';
+	
+	protected $modelName = 'Categorias\Model\Categoria';
+	
+	protected $routeName = 'categorias';
+	
+	public function getDataFromRequest()
     {
     	$codigo = $this->getRequest()->getPost('codigo');
     	$codigo_trabalho = $this->getRequest()->getPost('codigo_trabalho');
     	$data = $this->getRequest()->getPost('data');
     	$horario = $this->getRequest()->getPost('horario');
     	$codigo_local = $this->getRequest()->getPost('codigo_local');
-    	$grade = new Grade();
-    	$grade->exchangeArray(['codigo'=>$codigo,'codigo_trabalho'=>$codigo_trabalho,'data'=>$data,'horario'=>$horario,'codigo_local'=>$codigo_local]);
-    	$this->sm->get('GradeTable')->save($grade);
-    	return $this->redirect()->toRoute('grades');
+    	return [
+    			'codigo' => $codigo,
+    			'codigo_trabalho' => $codigo_trabalho,
+    			'data' => $data,
+    			'horario' => $horario,
+    			'codigo_local' => $codigo_local
+    	];
     }
-    
-    public function deleteAction()
-    {
-    	$codigo = $this->params('codigo');
-    	$this->sm->get('GradeTable')->delete($codigo);
-    	return $this->redirect()->toRoute('grades');
-    	
-    	
-    }
-    
 }
--- module/Grades/src/Model/Grade.php
+++ module/Grades/src/Model/Grade.php
@@ -1,22 +1,13 @@
 <?php
 namespace Grades\Model;
-class Grade
+
+use Application\Model\AbstractModel;
+
+class Grade extends AbstractModel
 {
 	public $codigo;
 	public $codigo_trabalho;
 	public $data;
 	public $horario;
 	public $codigo_local;
-	
-	public function exchangeArray($array)
-	{
-		foreach($array as $attribute => $value){
-			$this->$attribute = $value;
-		}
-	}
-	
-	public function toArray()
-	{
-		return get_object_vars($this);
-	}
 }
