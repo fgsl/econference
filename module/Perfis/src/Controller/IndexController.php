bfda260568daea6b3350341b79d1527ffb406786
aprendiz <aprendiz@serpro.gov.br>
2018-01-30 11:44:48 -0200

    Redução do módulo Perfis #99

--- module/Perfis/src/Controller/IndexController.php
+++ module/Perfis/src/Controller/IndexController.php
@@ -1,59 +1,32 @@
 <?php
 /**
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @link      http://github.com/fgsl/econference for the canonical source repository
+ * @copyright Copyleft 2017 FTSL. (http://www.ftsl.org.br)
+ * @license   https://www.gnu.org/licenses/agpl-3.0.en.html GNU Affero General Public License
  */
-
 namespace Perfis\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
-use Zend\View\Model\ViewModel;
-use Perfis\Model\Perfil;
+use Application\Controller\AbstractCrudController;
 
-class IndexController extends AbstractActionController
+class IndexController extends AbstractCrudController
 {
-	private $sm;
+	protected $mainTableFactory = 'PerfilTable';
 	
-	public function __construct($sm)
+	protected $rowsObjectName = 'perfis';
+	
+	protected $primaryKeyName = 'codigo';
+	
+	protected $modelName = 'Perfis\Model\Perfil';
+	
+	protected $routeName = 'perfis';
+	
+	public function getDataFromRequest()
 	{
-		$this->sm = $sm;
+		$codigo = $this->getRequest()->getPost('codigo');
+		$nome = $this->getRequest()->getPost('nome');
+		return [
+				'codigo' => $codigo,
+				'nome' => $nome
+		];
 	}
-	
-    public function indexAction()
-    {
-    	$perfis = $this->sm->get('PerfilTable')->getAll();
-        return new ViewModel(['perfis'=>$perfis]);
-    }
-    
-    public function editAction()
-    {
-    	$codigo = $this->params('codigo');
-    	if (is_null($codigo)){
-    		$perfil = new Perfil();
-    	} else {    	
-    		$perfil = $this->sm->get('PerfilTable')->getOne($codigo);
-    	}
-    	return new ViewModel(['perfil' => $perfil]);
-    }
-    
-    public function saveAction()
-    {
-    	$codigo = $this->getRequest()->getPost('codigo');
-    	$nome = $this->getRequest()->getPost('nome');
-    	$perfil = new Perfil();
-    	$perfil->exchangeArray(['codigo'=>$codigo,'nome'=>$nome]);
-    	$this->sm->get('PerfilTable')->save($perfil);
-    	return $this->redirect()->toRoute('perfis');
-    }
-    
-    public function deleteAction()
-    {
-    	$codigo = $this->params('codigo');
-    	$this->sm->get('PerfilTable')->delete($codigo);
-    	return $this->redirect()->toRoute('perfis');
-    	
-    	
-    }
-    
 }
--- module/Perfis/src/Model/Perfil.php
+++ module/Perfis/src/Model/Perfil.php
@@ -1,19 +1,10 @@
 <?php
 namespace Perfis\Model;
-class Perfil
+
+use Application\Model\AbstractModel;
+
+class Perfil extends AbstractModel
 {
 	public $codigo;
 	public $nome;
-	
-	public function exchangeArray($array)
-	{
-		foreach($array as $attribute => $value){
-			$this->$attribute = $value;
-		}
-	}
-	
-	public function toArray()
-	{
-		return get_object_vars($this);
-	}
 }
